4defea189a1d38e087e262bc45afc077b7962d02
albi nur <albi27076@gmail.com>
2024-06-11 23:41:15 +0700

    fix save semua dokumen

--- routes/save.js
+++ routes/save.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const Model_File = require("../model/Model_File");
+const Model_File = require("../model/Model_Files");
 const Model_Users = require("../model/Model_Users");
 const Model_Kategori = require("../model/Model_Kategori");
-const Model_Save = require("../model/Model_Save"); // Tambahkan model untuk tabel save
+const Model_Save = require("../model/Model_Save");
 
 router.get("/", async function (req, res, next) {
   let id = req.session.userId;
@@ -27,7 +27,7 @@ router.get("/create", async function (req, res, next) {
     let id = req.session.userId;
     let userData = await Model_Users.getId(id);
     let saveData = await Model_Save.getAll();
-    let fileData = await Model_File.getAll();
+    let fileData = await Model_Files.getAll();
     res.render("save/create", {
       users: userData[0],
       save: saveData,
@@ -43,22 +43,22 @@ router.post("/store", async function (req, res, next) {
   try {
     let { id_file } = req.body;
     let userData = await Model_Users.getId(req.session.userId);
-    let currentDate = new Date(); // Mengambil tanggal saat ini
+    let currentDate = new Date();
     let saveData = {
       id_user: req.session.userId,
       id_file,
       tanggal: currentDate.getDate(),
-      bulan: currentDate.getMonth() + 1, // Bulan dimulai dari 0, jadi tambahkan 1
+      bulan: currentDate.getMonth() + 1,
       tahun: currentDate.getFullYear(),
     };
     await Model_Save.Store(saveData);
 
-    req.flash("success", "Berhasil menyimpan data");
-    res.redirect("/save");
+    res.json({ success: true });
   } catch (error) {
     console.error("Error:", error);
-    req.flash("error", "Gagal menyimpan data");
-    res.redirect("/save");
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to save bookmark" });
   }
 });
 
